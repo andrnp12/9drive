27328b50b35574285e91e546d6c546cb9e302561
andrnp12 <andre.diko46@gmail.com>
2026-06-24 12:14:35 +0700

    fix refresh token

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -1,3 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient()
+// Membuat tipe untuk global agar TypeScript tidak komplain
+const globalForPrisma = global as unknown as { prisma: PrismaClient }
+
+export const prisma =
+  globalForPrisma.prisma || 
+  new PrismaClient({
+    log: ['error'], // Ubah ke ['query', 'error'] jika ingin melihat SQL di terminal
+  })
+
+// Simpan instance prisma ke dalam global hanya saat bukan di produksi
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
